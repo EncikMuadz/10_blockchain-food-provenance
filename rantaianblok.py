2e8f7db17be330646e624dda94bcabf18965f824
EncikMuadz <muadz@twistcode.com>
2020-07-06 04:53:37 +0100

    implement adding block, maklumat

--- rantaianblok.py
+++ rantaianblok.py
@@ -3,22 +3,33 @@
     attributes: senarai rantaian
     data-data semasa
 """
-
+from maklumat import Maklumat
+from blok import Blok
+from time import time
 class RantaianBlok:
     # attributes
     def __init__(self):
-        self.rantaian = []
+        self.rantaian = [] # mengandungi rantaian blok ditambah
+        self.maklumat_maklumat_semasa = [] # maklumat-maklumat terkini untuk ditambah ke blok
 
-    # metode untuk menambah blok
-    def penambah_blok(self):
-        pass
+    # menambah maklumat baharu
+    def menambah_maklumat(self):
+        id_organisasi = int(input())
+        id_penerima = int(input())
+        perkembangan = input()
+        maklumat_baru = Maklumat(id_organisasi, id_penerima, perkembangan) # instantiate objek kelas maklumat
+        self.maklumat_maklumat_semasa.append(maklumat_baru.penambah_maklumat()) # gabung ke list maklumat-maklumat semasa
+
+    # menambah blok baharu
+    def menambah_blok(self):
+        cincangan_sebelumnya = int(input())
+        pembuktian = input()
+        cetakan_masa = time()
+        blok_baharu = Blok(len(self.rantaian), cincangan_sebelumnya, self.maklumat_maklumat_semasa, pembuktian, cetakan_masa)
+        self.rantaian.append(blok_baharu.penambah_blok())
 
     # mencincang blok data ke dalam bentuk cryptic
     def pencincang(self):
-        pass
-
-    # memanggil/memapar blok terkini
-    def pemanggil_blok_terkini(self):
         pass
 
     # mendaftar organisasi yang berkepentingan
@@ -29,6 +40,6 @@ class RantaianBlok:
     def pengsahih_rantaian(self):
         pass
 
-    # membuktikan kebenaran data ditambah
+    # membuktikan kebenaran maklumat ditambah
     def pembukti(self):
         pass
