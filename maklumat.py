b93105e81b83777950f91558caccdd97dc0c15c6
EncikMuadz <muadz@twistcode.com>
2020-07-06 04:52:54 +0100

    return method penambah_maklumat

--- maklumat.py
+++ maklumat.py
@@ -1,12 +1,11 @@
 from collections import OrderedDict # gunapakai ordered dict(doubly linked list)
 
 """ Kelas objek Maklumat/Transaksi
-    organisasi bertanggungjawab akan mengemaskini status pada bahagian memasing
+    organisasi bertanggungjawab akan mengemaskini perkembangan pada bahagian memasing
 """
 class Maklumat:
     # attributes
     def __init__(self, organisasi_penambah, organisasi_penerima, perkembangan):
-        self.maklumat_maklumat_semasa = []
         self.organisasi_penambah = organisasi_penambah
         self.organisasi_penerima = organisasi_penerima
         self.perkembangan = perkembangan
@@ -14,10 +13,10 @@ class Maklumat:
     # mengarang maklumat baru untuk ditambah ke maklumat-maklumat semasa
     def penambah_maklumat(self):
         maklumat = OrderedDict(
-            {
+            [
                 ('organisasi_penambah', self.organisasi_penambah),
                 ('organisasi_penerima', self.organisasi_penerima),
                 ('perkembangan', self.perkembangan)
-            }
+            ]
         )
-        self.maklumat_maklumat_semasa.append(maklumat)
+        return maklumat
