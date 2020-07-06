7959604f0f71e06e089dbe6f64c4f8d28afc7463
EncikMuadz <muadz@twistcode.com>
2020-07-06 02:12:37 +0100

    add blockchain scaffold

--- rantaianblok.py
+++ rantaianblok.py
@@ -1,8 +1,39 @@
 """ Scaffold
-    blockchain class
+    kelas objek rantaianblok
+    attributes: senarai rantaian
+    data-data semasa
 """
 
 class RantaianBlok:
+    # attributes
     def __init__(self):
         self.rantaian = []
-        self.tetransaksi_semasa = []
+        self.data_data_semasa = []
+
+    # metode untuk menambah blok
+    def penambah_blok(self):
+        pass
+
+    # mengarang data baru untuk ditambah ke data-data semasa
+    def pengarang_data(self):
+        pass
+
+    # mencincang blok data ke dalam bentuk cryptic
+    def pencincang(self):
+        pass
+
+    # memanggil/memapar blok terkini
+    def pemanggil_blok_terkini(self):
+        pass
+
+    # mendaftar organisasi yang berkepentingan
+    def pendaftar_organisasi(self):
+        pass
+
+    # mengsahihkan rantaian blok
+    def pengsahih_rantaian(self):
+        pass
+
+    # membuktikan kebenaran data ditambah
+    def pembukti(self):
+        pass
