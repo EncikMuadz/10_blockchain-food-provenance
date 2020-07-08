512ca9315d248249450c96cf4af3f9e6d7819d08
EncikMuadz <muadz@twistcode.com>
2020-07-08 16:39:53 +0100

    implement; program loop using list comprehension

---
+++ program_jenis_repl.py
@@ -0,0 +1,9 @@
+program_masih_berjalan = True
+
+while program_masih_berjalan:
+    pilihan = input(
+        '\nInteraksi dengan program paparan CLI\n\tTekan \'1\': Tambah Maklumat\n\tTekan \'2\': Tambah blok maklumat\n\tTekan \'q\': Keluar dari program'
+    )
+    [print('WIP') for i in range(0,1) if pilihan == '1']
+    [print('WIP') for i in range(0,1) if pilihan == '2']
+    [exit() for i in range(0,1) if pilihan == 'q']
