263209dcac32e6b0f2db553139e6d45055ecd6ba
EncikMuadz <muadz@twistcode.com>
2020-07-26 15:23:02 +0100

    add; id generation using uuid4

--- nodus.py
+++ nodus.py
@@ -1,5 +1,19 @@
 import threading
+import socket
+from uuid import uuid4
 
 class Nodus(threading.Thread):
-    ''' Kelas ini blueprint kepada objek nodus'''
-    pass
+    ''' Kelas ini blueprint kepada objek nodus
+    
+        ip -> alamat ip/ host ip
+        port -> port dibuka
+    '''
+    def __init__(self, perumah, port):
+        threading.Thread.__init__(self)
+        self.perumah = perumah
+        self.port = port
+        self.id = str(uuid4().hex)
+        print(f'Bebenang kelas nodus dimulai pada, perumah: {self.perumah} dan port: {self.port}')
+
+    def jalan(self):
+        pass
