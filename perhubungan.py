f1d4c46b77a2a2ee3e06e2a0cd1c7dfc59b901c3
EncikMuadz <muadz@twistcode.com>
2020-07-30 00:56:31 +0100

    add constructor method

--- perhubungan.py
+++ perhubungan.py
@@ -4,7 +4,7 @@ class Perhubungan(threading.Thread):
     ''' kelas ini mewakili tcp/ip soket perhubungan antara dua nodus, menyimpan soket pelanggan dan id nodus berhubung
     
         komunikasi terjadi melalui kelas ini
-        mesej dihantar node berhubung disampaika kepada node utama yg mencipta objek Perhubungan
+        mesej dihantar node berhubung disampaikan kepada node utama yg mencipta objek Perhubungan
 
         Instantiate objek baru, mula/start bebenang
             nodus_utama -> nodus penerima perhubungan
@@ -13,3 +13,9 @@ class Perhubungan(threading.Thread):
             perumah -> alamat ip nodus utama
             port -> port nodus utama
     '''
+    def __init__(self, nodus_utama, soket, id, perumah, port):
+        self.perumah = perumah
+        self.port = port
+        self.nodus_utama = nodus_utama
+        self.soket = soket
+        self.penamat_flag = threading.Event
