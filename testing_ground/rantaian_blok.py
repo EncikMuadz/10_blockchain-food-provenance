aeb229b1ba4d70b90bad9d5996545d4d8634d636
EncikMuadz <muadz@twistcode.com>
2020-08-11 13:51:47 +0100

    added; using only one class, class Blok

--- testing_ground/rantaian_blok.py
+++ testing_ground/rantaian_blok.py
@@ -2,15 +2,20 @@ from hashlib import sha256
 
 class Blok:
     ''' mewakili kelas nodus didalam LinkedList'''
-    def __init__(self):
-        self.data = None
-        self.penerus = None
+    def __init__(self, data):
+        self.data = data
+        self.penerus = None # atau next element
 
-class Blockchain:
-    ''' mewakili kelas LinkedList'''
-    def __init__(self):
-        self.kepala = None
+# class Rantaian:
+#     ''' mewakili kelas LinkedList'''
+#     def __init__(self):
+#         self.kepala = None
+#     def attr_kep(self):
+#         return self.kepala
 
 mesej_awalan = 'Bacalah!!! dengan nama Tuhan mu..'
+mesej_dicincang = str(sha256(mesej_awalan.encode('UTF-8')).hexdigest())
+awalan = (mesej_awalan, mesej_dicincang)
 
-print(sha256(mesej_awalan.encode('UTF-8')).hexdigest())
+ujian = Blok(data = awalan)
+print(ujian.data)
