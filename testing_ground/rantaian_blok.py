5cec3290747c206a0f5210a8e8aeb4fbceb8b5f5
EncikMuadz <muadz@twistcode.com>
2020-08-11 13:23:28 +0100

    added; scaffold of the product from linked list data structure

---
+++ testing_ground/rantaian_blok.py
@@ -0,0 +1,10 @@
+class Blok:
+    ''' mewakili kelas nodus didalam LinkedList'''
+    def __init__(self):
+        self.data = None
+        self.penerus = None
+
+class Blockchain:
+    ''' mewakili kelas LinkedList'''
+    def __init__(self):
+        self.kepala = None
