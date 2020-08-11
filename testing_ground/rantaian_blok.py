2134798865651a1a7c960eabb64affc44de1620c
EncikMuadz <muadz@twistcode.com>
2020-08-11 13:30:43 +0100

    added; test how to encode string before hashing and outputting to the command line

--- testing_ground/rantaian_blok.py
+++ testing_ground/rantaian_blok.py
@@ -1,3 +1,5 @@
+from hashlib import sha256
+
 class Blok:
     ''' mewakili kelas nodus didalam LinkedList'''
     def __init__(self):
@@ -8,3 +10,7 @@ class Blockchain:
     ''' mewakili kelas LinkedList'''
     def __init__(self):
         self.kepala = None
+
+mesej_awalan = 'Bacalah!!! dengan nama Tuhan mu..'
+
+print(sha256(mesej_awalan.encode('UTF-8')).hexdigest())
