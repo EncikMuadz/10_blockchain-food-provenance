c2376c3192a38fd3d5302b13712d16b2c1c631f8
EncikMuadz <muadz@twistcode.com>
2020-08-14 17:15:39 +0100

    added, updated; pencincang_data, pemasuk_data, penghad_masukan_data

--- testing_ground/aliran_ringkas.py
+++ testing_ground/aliran_ringkas.py
@@ -1,20 +1,39 @@
 ''' Implementasi program repl rantaian blok tanpa kelas'''
 
+from hashlib import sha256
+
 # kontainer untuk rantaian blok sesi ini
 rantaian_data = []
+data_data = []
 
 def pemasuk_data():
     pengirim, penerima = input('\nMasukkan pengirim penerima dalam format berikut: Pengirim Penerima\n').split()
     mesej = input('\nTulis mesej anda:\n')
-    rantaian_data.append((pengirim, penerima, mesej))
+    data_data.append((pengirim, penerima, mesej))
+
+def penghad_masukan_data():
+    if len(data_data) == 3:
+        print('\nCincang data terlebih dahulu\n')
+
+def pencincang_data(data_data):
+    string_data_data = ''.join([str(elem) for elem in data_data])
+    cincangan = sha256(string_data_data.encode('utf-8')).hexdigest()
+    rantaian_data.append([data_data, cincangan])
+    data_data = [] # set kembali data_data kepada tiada
 
 def antara_muka():
     program_berjalan = True
     while program_berjalan:
         pilihan_pengguna = input()
         if pilihan_pengguna == 'm':
+            penghad_masukan_data()
             pemasuk_data()
         elif pilihan_pengguna == 'p':
+            for elem in data_data:
+                print(elem)
+        elif pilihan_pengguna == 'c':
+            pencincang_data(data_data)
+        elif pilihan_pengguna == 'rb':
             for elem in rantaian_data:
                 print(elem)
         elif pilihan_pengguna == 'q':
