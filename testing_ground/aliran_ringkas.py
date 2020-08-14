1c16dd6609378c18648ea63025e04e999b62f9a6
EncikMuadz <muadz@twistcode.com>
2020-08-14 13:55:12 +0100

    added pemasuk data, container for blockchain

--- testing_ground/aliran_ringkas.py
+++ testing_ground/aliran_ringkas.py
@@ -1,10 +1,18 @@
 ''' Implementasi program repl rantaian blok tanpa kelas'''
 
+# kontainer untuk rantaian blok sesi ini
+rantaian_data = []
+
+def pemasuk_data(pengirim, penerima, mesej):
+    pengirim, penerima = input('\nMasukkan pengirim penerima dalam format berikut: Pengirim Penerima\n').split()
+    mesej = input('\nTulis mesej anda:\n')
+    return pengirim, penerima, mesej
+
 def antara_muka():
     while True:
         pilihan_pengguna = input()
         if pilihan_pengguna == 'm':
-            print('\nMemasukkan data\n')
+            pemasuk_data()
         elif pilihan_pengguna == 'q':
             return False
         else:
