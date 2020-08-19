ba73117628902253a212f0a9afb326e64a5b9ced
EncikMuadz <muadz@twistcode.com>
2020-08-19 03:59:41 +0100

    patched, added; features to rantaianblok

--- testing_ground/rantaian_blok.py
+++ testing_ground/rantaian_blok.py
@@ -5,9 +5,10 @@ import json
 class Blok:
     ''' mewakili kelas nodus didalam LinkedList'''
     def __init__(self, indeks, data_data, cetakan_masa, cincangan_sebelum):
-        self.indeks = indeks
-        self.data_data = data_data
-        self.cetakan_masa = time() 
+        self.indeks = indeks # indeks blok ini
+        self.data_data = data_data # data-data
+        self.cetakan_masa = time() # masa berlaku
+        self.cincangan_blok = self.pencincang_blok() # cincangan blok ini
         self.cincangan_sebelum = cincangan_sebelum # atau next element
     
     def pencincang_blok(self):
@@ -17,8 +18,16 @@ class Blok:
 class Rantaian:
     ''' mewakili kelas LinkedList'''
     def __init__(self):
-        self.kepala = Blok(0, input(), time(), None)
         self.data_data_sesi = []
+        self.rantaian_data = []
+        self.kepala = Blok(0, self.data_data_sesi, time(), None)
+    
+    def pemula_kepala(self):
+        self.rantaian_data.append(self.kepala)
+
+    @property
+    def pemeriksa_blok_terkini(self):
+        return self.rantaian_data[-1]
 
 if __name__ == "__main__":
     ayat_awalan = 'Bacalah!!! dengan nama Tuhan mu..'
