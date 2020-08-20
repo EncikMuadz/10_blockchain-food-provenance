07464625aa8c769ff1fade8f8b56087def71a77f
EncikMuadz <muadz@twistcode.com>
2020-08-20 12:18:48 +0100

    updated; App components

--- testing_ground/rantaian-blok/src/App.js
+++ testing_ground/rantaian-blok/src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import RantaianBlokLogo from './blockchain_logo.svg';
 import './App.css';
+import Header from './components/header'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className="App-header-one">Rantaian</h1>
-        <img src={RantaianBlokLogo} className="App-logo" alt="rantaianbloklogo" />
-        <h1 className="App-header-two"> Blok</h1>
-      </header>
+      <Header />
       <body>
         <a className="App-body" href="https://twistcode.com" target="_blank" rel="noopener noreferrer"> Tekan untuk mula</a>
       </body>
