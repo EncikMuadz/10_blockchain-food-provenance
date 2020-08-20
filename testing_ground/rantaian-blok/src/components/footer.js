c4cd95fcc91c43ed0a0a88ddfb1f9bddf856dc15
EncikMuadz <muadz@twistcode.com>
2020-08-20 12:38:31 +0100

    added; footer component

---
+++ testing_ground/rantaian-blok/src/components/footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './css/footer.css';
+
+function Footer() {
+    return (
+    <div className="Footer">
+        <header className="footer">
+          <h1 className="h1-one">Copyright by:</h1>
+          <a className="footer-link" href="https://twistcode.com" target="_blank" rel="noopener noreferrer"> TheRantaianBlok@Twistcode</a>
+        </header>
+    </div>
+    );
+}
+
+export default Footer
