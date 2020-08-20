5a5dd16b4b4d5fe23412a6d4ec86f1f1948271ed
EncikMuadz <muadz@twistcode.com>
2020-08-20 12:40:12 +0100

    modified; App component, header component

--- testing_ground/rantaian-blok/src/App.js
+++ testing_ground/rantaian-blok/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/header'
+import Footer from './components/footer'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <body>
         <a className="App-body" href="https://twistcode.com" target="_blank" rel="noopener noreferrer"> Tekan untuk mula</a>
       </body>
+      <Footer />
     </div>
   );
 }
