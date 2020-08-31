d7e133eadc64f8b76906cd99db7d65afe5569d25
EncikMuadz <muadz@twistcode.com>
2020-08-31 10:01:56 +0100

    revert; to a normal component without rerouting ability

--- testing_ground/rantaian-blok/src/components/login.js
+++ testing_ground/rantaian-blok/src/components/login.js
@@ -1,29 +1,20 @@
 import React from 'react'
 import './css/login.css'
 import LoginButton from './login-button'
-import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
-import User from './user.js'
 
 function Login() {
     return (
-        <Router>
-            <form>
-                <h1>Log Masuk</h1>
-                <p>Organisasi
-                    <input className='organisasi'/>
-                </p>
-                <p>Kata Laluan
-                    <input className='kata-laluan'/></p>
-                <p>
-                    <Link to='/user'>
-                        <LoginButton />
-                    </Link>
-                </p>
-                <Switch>
-                    <Route path='/user' component={User} />
-                </Switch>
-            </form>
-        </Router>
+        <form>
+            <h1>Log Masuk</h1>
+            <p>Organisasi
+                <input className='organisasi'/>
+            </p>
+            <p>Kata Laluan
+                <input className='kata-laluan'/></p>
+            <p>
+                <LoginButton />
+            </p>
+        </form>
     );
 }
 
