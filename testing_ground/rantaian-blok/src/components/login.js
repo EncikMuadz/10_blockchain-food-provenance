a7bf27c08f96ec927fb5168a2b58077da12c6409
EncikMuadz <muadz@twistcode.com>
2020-09-01 05:53:29 +0100

    added, updated; using hook to receive input from the User component form

--- testing_ground/rantaian-blok/src/components/login.js
+++ testing_ground/rantaian-blok/src/components/login.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import './css/login.css'
 import LoginButton from './login-button'
+import { useInput } from './hooks/useInput'
 
-function Login() {
+function Login(props) {
+    const { value:organisasi, bind:bindOrganisasi, reset:resetOrganisasi } = useInput('')
+    const { value:katalaluan, bind:bindKataLaluan, reset:resetKataLaluan } = useInput('')
+    
+    const handleSubmit = (evt) => {
+        evt.preventDefault()
+        alert(`Dalam proses log masuk ${organisasi} ${katalaluan}`)
+        resetOrganisasi()
+        resetKataLaluan()
+    }
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <h1>Log Masuk</h1>
             <p>Organisasi
-                <input className='organisasi'/>
+                <input className='organisasi' type='text' {...bindOrganisasi}/>
             </p>
             <p>Kata Laluan
-                <input className='kata-laluan'/></p>
+                <input className='kata-laluan' type='text' {...bindKataLaluan}/></p>
             <p>
                 <LoginButton />
             </p>
