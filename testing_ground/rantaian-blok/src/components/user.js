a817be7b84a0ac820b2a7475ccb20f5e9c51e800
EncikMuadz <muadz@twistcode.com>
2020-09-01 06:16:43 +0100

    updated; receiving input for User component

--- testing_ground/rantaian-blok/src/components/user.js
+++ testing_ground/rantaian-blok/src/components/user.js
@@ -1,19 +1,30 @@
 import React from 'react'
 import './css/user.css'
 import UserButton from './user-button'
+import { useInput } from './hooks/useInput'
 
-function User() {
+function User(props) {
+    const { value: pengirim, bind:bindPengirim, reset:resetPengirim } = useInput('')
+    const { value: penerima, bind:bindPenerima, reset:resetPenerima } = useInput('')
+    const { value: data, bind:bindData, reset:resetData } = useInput('')
+    const handleSubmit = (evt) => {
+        evt.preventDefault()
+        alert(`Menghantar ${data} daripada ${pengirim} kepada ${penerima}`)
+        resetPengirim()
+        resetPenerima()
+        resetData()
+    }
     return(
-        <form className='user-form'>
+        <form className='user-form' onSubmit={handleSubmit}>
             <h1 className='h1-form'>Hantar data terkini</h1>
             <p>Pengirim
-                <input className='pengirim-form'/>
+                <input className='pengirim-form' type='text' {...bindPengirim}/>
             </p>
             <p>Penerima
-                <input className='penerima-form'/>
+                <input className='penerima-form' type='text' {...bindPenerima}/>
             </p>
             <p>Data
-                <input className='data-input'/>
+                <input className='data-input' type='text' {...bindData}/>
             </p>
             <p>
                 <UserButton />
