e9b8e944a858dd7d0b4314a1a3fe533d00ea2887
EncikMuadz <muadz@twistcode.com>
2020-09-01 02:53:04 +0100

    updated; css formatting for User component

--- testing_ground/rantaian-blok/src/components/user.js
+++ testing_ground/rantaian-blok/src/components/user.js
@@ -4,11 +4,16 @@ import './css/user.css'
 function User() {
     return(
         <form className='user-form'>
-            <h1>Gunalah</h1>
-            <p>Data</p>
-            <input className='data-input'/>
-            <p>Penerima</p>
-            <input className='penerima-form'/>
+            <h1 className='h1-form'>Hantar data terkini</h1>
+            <p>Pengirim
+                <input className='pengirim-form'/>
+            </p>
+            <p>Penerima
+                <input className='penerima-form'/>
+            </p>
+            <p>Data
+                <input className='data-input'/>
+            </p>
         </form>
     );
 }
